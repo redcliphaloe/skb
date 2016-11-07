b448f41ff8334b04d83ca2d4ddae52880ab22501
redcliphaloe <redcliphaloe@gmail.com>
2016-11-08 01:09:28 +0300

    skb-startAdd

---
+++ skb-start/index.js
@@ -0,0 +1,4 @@
+var Skb = require('skb');
+var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ODE4ZWU0MjJmYjc0ZDAwMTFiZTgyYzQiLCJ1c2VybmFtZSI6InJlZGNsaXBoYWxvZUBnbWFpbC5jb20iLCJyb2xlIjoidXNlciIsImlhdCI6MTQ3ODAyODg3MH0.fNJVhZ0NWxYZ6w4XuNHDsV4ejAZM0Zyx9SzuG7-2l9Q';
+var skb = new Skb(token);
+skb.taskHelloWorld('redcliphaloe');
