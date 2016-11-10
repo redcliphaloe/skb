f263e0e36e8d657a6cb92a12fd5fcf5d251f3f65
redcliphaloe <redcliphaloe@gmail.com>
2016-11-10 14:48:58 +0300

    skb2Add2C

--- skb2/src/index.js
+++ skb2/src/index.js
@@ -6,43 +6,45 @@ const app = express();
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.json({
-    hello: 'JS World',
+	res.json({
+		hello: 'JS World',
   });
 });
 
 app.get('/task2A', (req, res) => {
 	const sum = (+req.query.a || 0) + (+req.query.b || 0);
-  res.send(sum + '');
+
+	res.send(sum + '');
 });
 
-function formatFullName(fullName) {
-	let fullNameFormatted = '';		
-	// если имя не пустое
-	if (fullName) {
-		const re = /\d|_|\//g; 
-		// если в имени нет цифр или подчеркивания или слеша
-		if (!~fullName.search(re)) { 
-			const fullNameParts = fullName.trim().split(/\s+/); // массив без пустых элементов 
-			const fullNamePartsLength = fullNameParts.length;
-			// если имя больше ноля и меньше четырех слов
-			if (fullNamePartsLength > 0 && fullNamePartsLength < 4) {
-				fullNameFormatted = fullNameParts[fullNamePartsLength - 1].toLowerCase(); // все буквы в нижнем регистре
-				fullNameFormatted = fullNameFormatted[0].toUpperCase() + fullNameFormatted.slice(1); // а первая в верхнем
-				for (let i = 0; i <= fullNamePartsLength - 2; i++) {
-					fullNameFormatted += ' ' + fullNameParts[i][0].toUpperCase() + '.'; // буквы в верхнем регистре
-				}
-			}
+function formatFullname(fullname) {
+	let fullnameFormatted = '';	
+	const fullnameParts = fullname.trim().split(/\s+/);
+
+	if (fullname && (!~fullname.search(/\d|_|\//)) && (fullnameParts.length > 0) && (fullnameParts.length < 4)) {
+		fullnameFormatted = fullnameParts[fullnameParts.length - 1].toLowerCase();
+		fullnameFormatted = fullnameFormatted[0].toUpperCase() + fullnameFormatted.slice(1);
+
+		for (let i = 0; i <= fullnameParts.length - 2; i++) {
+			fullnameFormatted += ' ' + fullnameParts[i][0].toUpperCase() + '.';
 		}
 	}
 
-	return fullNameFormatted || 'Invalid fullname';
+	return fullnameFormatted || 'Invalid fullname';
 }
 
 app.get('/task2B', (req, res) => {
-  res.send(formatFullName(req.query.fullname));
+	res.send(formatFullname(req.query.fullname));
 });
 
+function usernameFromURL(url) {
+	return url[0] === '@' ? url : '@' + url.slice(url.lastIndexOf('/') + 1);	
+}
+
+app.get('/task2C', (req, res) => {
+	res.send(usernameFromURL(req.query.username));
+  });
+
 app.listen(3000, () => {
-  console.log('Your app listening on port 3000!');
+	console.log('Your app listening on port 3000!');
 });
