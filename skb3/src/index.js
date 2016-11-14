c2c6e1f27cd539d5c09f6e79ee55904e3a5fa1ce
redcliphaloe <redcliphaloe@gmail.com>
2016-11-14 23:17:17 +0300

    skb3Update39Tests

--- skb3/src/index.js
+++ skb3/src/index.js
@@ -9,6 +9,7 @@ app.use(cors());
 const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
 
 let pc = {};
+
 fetch(pcUrl)
   .then(async (res) => {
     pc = await res.json();
@@ -17,53 +18,68 @@ fetch(pcUrl)
     console.log('Что-то пошло не так:', err);
   });
 
+const errorNotFound = {
+	"404": "Not Found"
+}
+
 app.get('/task3A', (req, res) => {
 	res.json(pc);  
 });
 
-//get object
-app.get('/task3A/:key', async (req, res) => {
-    let objKey = req.params.key;
-    (pc[objKey]) ? objKey : objKey = objKey.toString();
-    let reply = pc[objKey];
-    
-    (reply != null && reply.constructor === Array)
-        ? reply = pc[objKey.toString()]
-        : reply;
-    
-    await (reply && reply !== 'array')
-        ? res.json(reply) 
-        : (pc[objKey] === 0) 
-                ? res.send('0') 
-                : (pc[objKey] === null) 
-                        ? res.json(reply) 
-                        : res.sendStatus(404);
-    
-    console.log(objKey + ' - ' + reply);
+app.get('/task3A/volumes', (req, res) => {
+	let dataFound = {};
+	let hdd = pc['hdd'] || [];
+
+	for (let i = 0; i < hdd.length; i++) {
+		if (!dataFound[hdd[i]['volume']]) {
+			dataFound[hdd[i]['volume']] = hdd[i]['size'];
+		} else {
+			dataFound[hdd[i]['volume']] += hdd[i]['size'];
+		}
+	}
+
+	for (let p in dataFound) {
+		dataFound[p] = dataFound[p] + 'B';
+	}
+
+	hdd.length > 0 ? res.json(dataFound) : res.sendStatus(404);
+	//res.json(hdd.length > 0 ? dataFound : errorNotFound);
 });
 
-//get field
-app.get('/task3A/:key/:value', async (req, res) => {
-    const objKey = req.params.key;
-    let objValue;
-    
-    (req.params.value === 'length') ? res.sendStatus(404) : objValue = req.params.value;
-    
-    console.log(objKey + '/' + objValue);
-    
-    (pc[objKey] === undefined) ? res.sendStatus(404) : 0;
-    (pc[objKey][objValue] === undefined) ? res.sendStatus(404) : 0;
-    
-    let reply = pc[objKey][objValue];
-    console.log(reply);
-    
-    (pc[objKey] === Array)
-        ? reply = 'array'
-        : reply;
-    
-    await (reply && reply !== 'array')
-        ? res.json(reply) 
-        : res.sendStatus(404);
+app.get('/task3A/:key', (req, res) => {
+	let dataFound;
+
+	try {
+		if (req.params.value != 'length') {
+			dataFound = pc[req.params.key];
+		}		
+	} finally {
+		dataFound !== undefined ? res.json(dataFound) : res.sendStatus(404);
+	}
+});
+
+app.get('/task3A/:key/:value', (req, res) => {
+	let dataFound;
+
+	try {
+		if (req.params.value != 'length') {
+			dataFound = pc[req.params.key][req.params.value];
+		}		
+	} finally {
+		dataFound !== undefined ? res.json(dataFound) : res.sendStatus(404);
+	}
+});
+
+app.get('/task3A/:key/:value/:value2', (req, res) => {
+	let dataFound;
+
+	try {
+		if (req.params.value2 != 'length') {
+			dataFound = pc[req.params.key][req.params.value][req.params.value2];
+		}				
+	} finally {
+		dataFound !== undefined ? res.json(dataFound) : res.sendStatus(404);
+	}	
 });
 
 app.listen(3000, () => {
