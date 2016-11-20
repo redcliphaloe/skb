f2e431bd7090bfb0b9eeecef8d9ac9f0e005bbd5
redcliphaloe <redcliphaloe@gmail.com>
2016-11-21 02:09:11 +0300

    skb2DUpdate41Tests

--- skb2/src/index.js
+++ skb2/src/index.js
@@ -87,6 +87,131 @@ app.get('/task2C', (req, res) => {
 	res.send(usernameFromURL(req.query.username));
   });
 
+/*
+Задача 2D: #colors
+Краткое описание задачи
+Распознать цвет и привести его к #abcdef виду
+Полное описание задачи
+Довольно часто, разработчиком приходится работать с цветами. Существуем множество цветовых систем и форматов их записи.
+Клиент выполняет GET запрос с параметром Query: ?color= и присылает цвета в разных форматах.
+Задача: привести все цвета к виду HEX виду в нижнем регистре: #123abc.
+В случае если в color находится некорректный цвет, Invalid color
+*/
+
+function hslToRgb(h, s, l) {
+	let result = {};
+	result.r = result.g = result.b = undefined;
+	let r, g, b;	
+
+	s = s.replace(/%/, '');
+	l = l.replace(/%/, '');
+
+	if (0 <= h && h <= 360 && 0 <= s && s <= 100 && 0 <= l && l <= 100) {
+		h = h / 360;
+		s = s / 100;
+		l = l / 100;
+
+		if (s == 0) {
+			r = g = b = l;
+		} else {
+			const hue2rgb = function hue2rgb(p, q, t){
+				if (t < 0) t += 1;
+				if (t > 1) t -= 1;
+				if (t < 1/6) return p + (q - p) * 6 * t;
+				if (t < 1/2) return q;
+				if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+			
+				return p;
+			}
+
+			let q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+			let p = 2 * l - q;
+		
+			r = hue2rgb(p, q, h + 1/3);
+			g = hue2rgb(p, q, h);
+			b = hue2rgb(p, q, h - 1/3);
+		}
+
+		result.r = Math.round(r * 255);
+		result.g = Math.round(g * 255);
+		result.b = Math.round(b * 255);
+	}
+
+	return result;
+}
+
+function hslStringToHex(hslString) {
+	let result = 'Invalid color';
+
+	hslString.replace(/hsl\((-?\d+),(-?\d+%),(-?\d+%)\)/, function (hslString, h, s, l) {
+		const rgb = hslToRgb(h, s, l);
+		result = rgbToHex(rgb.r, rgb.g, rgb.b);
+	});
+
+	return result;
+}
+
+function rgbToHex(r, g, b) {
+	let result = 'Invalid color';
+
+	if (0 <= r && r <= 255 && 0 <= g && g <= 255 && 0 <= b && b <= 255) {
+		r = Number(r).toString(16); 
+		g = Number(g).toString(16);
+		b = Number(b).toString(16);
+		r += r.length == 1 ? r : '';
+		g += g.length == 1 ? g : '';
+		b += b.length == 1 ? b : '';
+		result = r + g + b;
+	}
+
+	return result;
+}
+
+function rgbStringtoHex(rgbString) {
+	let result = 'Invalid color';
+
+	rgbString.replace(/rgb\((-?\d+),(-?\d+),(-?\d+)\)/, function (rgbString, r, g, b) {
+		result = rgbToHex(r, g, b);
+	});
+
+	return result;
+}
+
+function colorToHexFromQuery(query) {
+	if (!query.color) {
+		return 'Invalid color';
+	}
+
+	let result = query.color.replace(/ |%20/g, '').toLowerCase();
+
+	if (result.indexOf('rgb') == 0) {
+		result = rgbStringtoHex(result);
+	}
+
+	if (result.indexOf('hsl') == 0) {
+		result = hslStringToHex(result);
+	}
+
+	if (result[0] == '#') {
+		result = result.slice(1);
+	}
+
+	if (result.length == 3) {
+		result = result[0] + result[0] + result[1] + result[1] + result[2] + result[2];
+	}
+
+	if (~result.search(/[^\da-fA-F]/) || result.length != 6) {
+		return 'Invalid color';
+	}
+
+	return '#' + result;
+}
+
+app.get('/task2D', (req, res) => {
+	console.log(req.query);
+	res.send(colorToHexFromQuery(req.query));
+});
+
 app.listen(3000, () => {
 	console.log('Your app listening on port 3000!');
 });
